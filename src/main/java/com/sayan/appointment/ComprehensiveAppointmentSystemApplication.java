1cf26762f89cd5f8765c515584888f7b48810794
mahdi <mahdi773148@gmail.com>
2025-06-27 12:42:06 +0330

    add initializr spring boot and h2 console

---
+++ src/main/java/com/sayan/appointment/ComprehensiveAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sayan.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprehensiveAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprehensiveAppointmentSystemApplication.class, args);
+	}
+
+}
